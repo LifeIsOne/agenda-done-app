310ef2ec021a9cedf0ce9133c5eec9c9b7ff80c5
LifeIsOne <didtmdgh258@gmail.com>
2025-09-11 19:15:11 +0900

    [unplugin] 의존성 추가, 설정

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      dts: true, // [TS] 사용시 활성화 기본
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
